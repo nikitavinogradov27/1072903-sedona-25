861de2427be76dd3f3a168e8f0346e7dda96f7af
nikitavinogradov27 <venik1810@yandex.ru>
2019-06-22 17:30:33 +0300

    exam_2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
-$(document).ready(function() {
-  $('.minus').click(function () {
-    var $input = $(this).parent().find('input');
-    var count = parseInt($input.val()) - 1;
-    count = count < 1 ? 1 : count;
-    $input.val(count);
-    $input.change();
-    return false;
-  });
-  $('.plus').click(function () {
-    var $input = $(this).parent().find('input');
-    $input.val(parseInt($input.val()) + 1);
-    $input.change();
-    return false;
-  });
-});
+
+var open = document.querySelector(".modal_search");
+var btn = document.querySelector(".button_search_interactive");
+btn.addEventListener ('click', function () {
+    console.log('Клик по кнопке');
+    open.classList.add('modal_');
+}
+);
